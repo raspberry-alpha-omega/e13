860a073689266a0b85a40e223d16e28d7b83c8fc
Frank Carver <frank@efsol.com>
2013-01-15 11:10:06 +0000

    added dynamic word building

--- src/e13.c
+++ src/e13.c
@@ -11,10 +11,11 @@ typedef void (*typefn)(struct Dent* dent);
 struct Dent {
 	const char* name;
 	typefn type;
-	union { void* p; uint32_t v; } value;
+	union { void* p; int v; } value;
 	struct Dent* prev;
 };
 
+void nop_fn(struct Dent* dent) {}
 
 void fn_fn(struct Dent* dent) {
 	((fn)dent->value.p)();
@@ -28,26 +29,29 @@ void hello(void) {
 	puts("hello");
 }
 
-void goodbye(void) {
-	puts("goodbye");
+struct Dent dict[1000] = {
+		{ "", nop_fn, 0, 0 }
+};
+struct Dent* head = dict;
+
+void dadd_p(const char* name, typefn type, void* p) {
+	struct Dent* dent = head+1;
+	dent->name = name;
+	dent->type = type;
+	dent->value.p = p;
+	dent->prev = head;
+	++head;
 }
 
-struct Dent dict[1000] = {
-		{ },
-		{ "?", fn_fn, {.p = &hello}, 0 },
-		{ "p", prints_fn, {.p = "lala"}, 1}
-};
-int head = 2;
 
 void eval(const char* word) {
-	int walk = head;
+	struct Dent* walk = head;
 	do {
-		struct Dent* def = &dict[walk];
-		if (strcmp(word, def->name) == 0) {
-			def->type(def);
+		if (strcmp(word, walk->name) == 0) {
+			walk->type(walk);
 			break;
 		}
-		walk = def->prev;
+		walk = walk->prev;
 	} while (walk != 0);
 
 	if (0 == walk) {
@@ -56,6 +60,9 @@ void eval(const char* word) {
 }
 
 int main(void) {
+	dadd_p("?", fn_fn, &hello);
+	dadd_p("p", prints_fn, "lala");
+
 	eval("?");
 	eval("p");
 	eval("q");
