326ec2aec0cb47cac5ec9d42ea123e402889ea59
Frank Carver <frank@efsol.com>
2013-02-22 10:44:59 +0000

    a bit of annotation in assembler start code

--- src/start_x86_64.s
+++ src/start_x86_64.s
@@ -2,12 +2,15 @@ wordsize = 8
 
 .globl _start
 .globl memory_start
+.extern main
 
+.text
 _start:
   call main
   movl $1, %eax
   xor %ebx, %ebx
   int $0x80
 
+.data
 .align wordsize
 memory_start:
